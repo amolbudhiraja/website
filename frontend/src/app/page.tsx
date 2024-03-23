022a58cd4fe286b3ce3b3cf748e51b2a2c5a0ca3
Amol Budhiraja <abudhiraja@berkeley.edu>
2024-03-23 15:58:10 -0700

    Organized Home Header Code

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -9,18 +9,25 @@ export default function Home() {
   return (
     <>
       <NavBar />
-      <div className="px-10 lg:px-20">
-        <div className="flex flex-col lg:flex-row items-center">
-          <div className="m-5">
-            <Image src={memoji} alt="Memoji of Amol Budhiraja" className="h-25 border-4 rounded-full border-blue-300"/>
-          </div>
-          <div className="text-center lg:text-start">
-            <h1 className={`text-2xl font-bold ${gradientPatern}`}>Hi! I am Amol Budhiraja!</h1>
-            <p className={`${textColorDarkMode} md:w-4/6`}>I am studying <a href="https://eecs.berkeley.edu/" className={linkFormat}>EECS</a> @ <a href="https://www.berkeley.edu/" className={linkFormat}>Berkeley</a>. I am also researching the intersection of LLMs and IoT devices at the <a href="https://netsys.cs.berkeley.edu/" className={linkFormat}>Berkeley NetSys Lab</a>. Additionally, I am a product manager and developer at <a href="https://mdb.dev/" className={linkFormat}>Mobile Developers of Berkeley</a>.</p>
-          </div>
-        </div>
-      </div>
+      <HomeHeader />
       <Footer />
     </>
   );
+}
+
+/** Introduction section where I introduce myself and link some of my affiliations. */
+const HomeHeader = () => {
+  return (
+    <div className="px-10 lg:px-20">
+    <div className="flex flex-col lg:flex-row items-center">
+      <div className="m-5">
+        <Image src={memoji} alt="Memoji of Amol Budhiraja" className="h-25 border-4 rounded-full border-blue-300"/>
+      </div>
+      <div className="text-center lg:text-start">
+        <h1 className={`text-2xl font-bold ${gradientPatern}`}>Hi! I am Amol Budhiraja!</h1>
+        <p className={`${textColorDarkMode} md:w-4/6`}>I am studying <a href="https://eecs.berkeley.edu/" className={linkFormat}>EECS</a> @ <a href="https://www.berkeley.edu/" className={linkFormat}>Berkeley</a>. I am also researching the intersection of LLMs and IoT devices at the <a href="https://netsys.cs.berkeley.edu/" className={linkFormat}>Berkeley NetSys Lab</a>. Additionally, I am a product manager and developer at <a href="https://mdb.dev/" className={linkFormat}>Mobile Developers of Berkeley</a>.</p>
+      </div>
+    </div>
+  </div>
+  ); 
 }
