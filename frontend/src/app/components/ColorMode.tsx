620693e8c61114977e65089210c1d64b14a44b90
Amol Budhiraja <abudhiraja@berkeley.edu>
2024-03-23 15:40:47 -0700

    Created Home Page Introduction Header

---
+++ frontend/src/app/components/ColorMode.tsx
@@ -0,0 +1,12 @@
+// TailwindCSS Color Modes
+
+/* DARK MODE COLOR ELEMENTS */
+export const textColorDarkMode = 'text-white'; 
+
+/* LIGHT MODE COLOR ELEMENTS */
+
+/* GRADIENT COLOR ELEMENTS */
+export const gradientPatern = 'text-transparent bg-clip-text bg-gradient-to-br from-green-300 to-blue-400';
+
+/* UNIVERSAL COLOR ELEMENTS */
+export const linkFormat = 'text-blue-300 hover:underline'; 
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,24 @@
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import { textColorDarkMode, gradientPatern, linkFormat } from "./components/ColorMode";
+import Image from "next/image";
+import memoji from "./media/memoji.png"
+
 
 export default function Home() {
   return (
     <>
       <NavBar />
       <div className="px-10 lg:px-20">
-        <p>Hello World</p>
+        <div className="flex flex-col lg:flex-row items-center">
+          <div className="m-5">
+            <Image src={memoji} alt="Memoji of Amol Budhiraja" className="h-25 border-4 rounded-full border-blue-300"/>
+          </div>
+          <div className="text-center lg:text-start">
+            <h1 className={`text-2xl font-bold ${gradientPatern}`}>Hi! I am Amol Budhiraja!</h1>
+            <p className={`${textColorDarkMode} md:w-4/6`}>I am studying <a href="https://eecs.berkeley.edu/" className={linkFormat}>EECS</a> @ <a href="https://www.berkeley.edu/" className={linkFormat}>Berkeley</a>. I am also researching the intersection of LLMs and IoT devices at the <a href="https://netsys.cs.berkeley.edu/" className={linkFormat}>Berkeley NetSys Lab</a>. Additionally, I am a product manager and developer at <a href="https://mdb.dev/" className={linkFormat}>Mobile Developers of Berkeley</a>.</p>
+          </div>
+        </div>
       </div>
       <Footer />
     </>
